0b238daa0e6b2c2682fe6d2ab891414664e030c9
jmpcode <jan.kernel@gmail.com>
2014-10-24 01:05:43 +0530

    heading added to the content, version number changed

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='nerdcraw',
-      version='0.3',
+      version='0.3.1.2',
       description='Read slashdot.org without leaving\
       the terminal',
       classifiers=[
